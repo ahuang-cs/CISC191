c190d2812d4a7613c6a96cee5d1d5906c94f2e89
LouDotimas <61715466+LouDotimas@users.noreply.github.com>
2020-03-08 23:57:30 -0700

    Update lab1.java

--- lab1/src/main/java/edu/sdccd/cisc191/lab1.java
+++ lab1/src/main/java/edu/sdccd/cisc191/lab1.java
@@ -3,5 +3,5 @@ package edu.sdccd.cisc191;
 public class lab1 {
     public static void main(String[] args) {
         System.out.println("Hello World"); //Louie Dotimas
-    }
-}
+    }//end main
+}//end cls
